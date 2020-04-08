412cd6dcc07d599d323ee2d6ec9f7c8b6f47bc42
Oliver Sun <olisun@mac.com>
2020-04-08 13:10:02 -0700

    added a custom service for pulling an avatar for the user in uesr.class.js

--- backend/src/services/users/users.class.js
+++ backend/src/services/users/users.class.js
@@ -1,5 +1,19 @@
 const { Service } = require('feathers-mongoose');
+const crypto = require('crypto');
+const gravatarURL = 'https://s.gravatar.com/avatar';
+const query = 's=60';
 
 exports.Users = class Users extends Service {
-  
+  create(data, params) {
+    const { email, password, githubId } = data;
+    const hash = crypto.createHash('md5').update(email.toLowerCase().digest('hex'));
+    const avatar = `${gravatarURL}/${hash}?${query}`;
+    const useData = {
+      email,
+      password,
+      githubId,
+      avatar
+    };
+    return super.create(userData, params);
+  }
 };
